f458a099f5825fb3b8e4289a976fee97884fe5e8
Mike Mansour <mnmansour91@gmail.com>
2019-10-28 16:24:53 -0700

    PC-2019.2.3 <michael_mansour@M267328RCG8WP Create baseRefactoring.xml, markdown.xml, ide.general.xml, rainbow_brackets.xml, power.mode.ii.xml, Pytest.py, Default.xml, ignore.xml, find.xml, log_highlighting.xml, filetypes.xml, Python Script.py, colors.scheme.xml, KeyPromoterXSettings.xml, notifications.xml, _@user_Darcula.icls, github.xml, GrepConsole.xml, jdk.table.xml, laf.xml, material_theme.xml, editor.codeinsight.xml, keymap.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/Pytest.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Michael Mansour; (michael_mansour@symantec.com)'
+
+
+import pytest
+
---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Michael Mansour (michael_mansour@symantec.com)"
